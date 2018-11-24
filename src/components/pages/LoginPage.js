d292372603bb05e6b3f1456c862b76e04f2d70e0
Rajesh Sambasivam <rajbca00@gmail.com>
2018-11-24 07:19:09 +0530

    login form created

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,8 +1,17 @@
-import React from 'react';
-const LoginPage = () => (
-    <div>
-        <h1>Login Page</h1>
-    </div>
-);
+import React, { Component } from 'react';
+import LoginForm from '../forms/LoginForm';
+class LoginPage extends Component {
+    submit = data => {
+        console.log(data);
+    };
+    render() {
+        return (
+            <div>
+                <h1>Login Page</h1>
+                <LoginForm submit={this.submit} />
+            </div>
+        );
+    }
+}
 
 export default LoginPage;
